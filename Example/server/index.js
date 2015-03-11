72b351e38e0b798b7ca36d59fda34fd665a6fbfa
banjun <banjun@gmail.com>
2015-03-12 00:06:51 +0900

    add example socket.io server like http://socket.io/get-started/chat/

---
+++ Example/server/index.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+    socket.on('message', function(msg){
+        socket.emit('message', "echo: " + msg);
+    });
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+
