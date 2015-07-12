fd66a9fa622f0825c4d1c4f3aba3cb6db422b0cf
NachoSoto <ignaciosoto90@gmail.com>
2015-07-12 00:58:09 +0000

    Created project with iOS and Mac targets

---
+++ SocketIO-MacTests/SocketIO_MacTests.swift
@@ -0,0 +1,36 @@
+//
+//  SocketIO_MacTests.swift
+//  SocketIO-MacTests
+//
+//  Created by Nacho Soto on 7/11/15.
+//
+//
+
+import Cocoa
+import XCTest
+
+class SocketIO_MacTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
