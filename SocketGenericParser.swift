5d9a8e88bf0b47ee492df7468efc643bad61bd83
Erik <nuclear.ace@gmail.com>
2015-09-07 17:56:53 -0400

    don't need to specifiy get

--- SocketGenericParser.swift
+++ SocketGenericParser.swift
@@ -12,20 +12,18 @@ struct SocketGenericParser {
     let message: String
     var currentIndex: Int
     var messageCharacters: [Character] {
-        get {
-            return Array(message.characters)
-        }
-    }
-    var currentCharacter: String? {
-        get{
-            if currentIndex >= messageCharacters.count {
-                return nil
-            }
-            return String(messageCharacters[currentIndex])
-        }
+        return Array(message.characters)
     }
     
-    mutating func read(characterLength:Int) -> String? {
+    var currentCharacter: String? {
+        if currentIndex >= messageCharacters.count {
+            return nil
+        }
+        
+        return String(messageCharacters[currentIndex])
+    }
+    
+    mutating func read(characterLength: Int) -> String? {
         let startIndex = message.startIndex.advancedBy(currentIndex)
         let range = Range<String.Index>(start: startIndex, end: startIndex.advancedBy(characterLength))
         currentIndex = currentIndex + characterLength
@@ -33,7 +31,7 @@ struct SocketGenericParser {
         return message.substringWithRange(range)
     }
     
-    mutating func readUntilStringOccurence(string:String) -> String? {
+    mutating func readUntilStringOccurence(string: String) -> String? {
         let startIndex = message.startIndex.advancedBy(currentIndex)
         let range = Range<String.Index>(start: startIndex, end: message.endIndex)
         let subString = message.substringWithRange(range) as NSString
@@ -46,7 +44,7 @@ struct SocketGenericParser {
         return subString.substringToIndex(foundRange.location)
     }
     
-    mutating func readUntilEnd() ->String {
+    mutating func readUntilEnd() -> String {
         return read(messageCharacters.count - currentIndex)!
     }
 }
