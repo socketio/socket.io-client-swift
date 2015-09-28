66455f3e1b657c8dd8b3ce3b6c44f520fb53f285
Erik <nuclear.ace@gmail.com>
2015-09-28 14:37:51 -0400

    add namespace polling ack test

---
+++ SocketIO-MacTests/SocketPollingNamespaceAcknowledgementTest.swift
@@ -0,0 +1,18 @@
+//
+//  SocketPollingNamespaceAcknowledgementTest.swift
+//  Socket.IO-Client-Swift
+//
+//  Created by Erik Little on 9/28/15.
+//
+//
+
+import Foundation
+import XCTest
+
+class SocketPollingNamespaceAcknowledgementTest: SocketNamespaceAcknowledgementTest {
+    override func setUp() {
+        AbstractSocketTest.socket = AbstractSocketTest.namespacePollingSocket
+        testKind = TestKind.Acknowledgement
+        openConnection(AbstractSocketTest.socket)
+    }
+}
