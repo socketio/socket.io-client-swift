1ad87bdb0d8766e567f15e1802e3a62633f8ecc3
Erik <nuclear.ace@gmail.com>
2015-09-28 13:41:05 -0400

    add polling ack tests

---
+++ SocketIO-MacTests/SocketPollingAcknowledgementTest.swift
@@ -0,0 +1,18 @@
+//
+//  SocketPollingAcknowledgementTest.swift
+//  Socket.IO-Client-Swift
+//
+//  Created by Erik Little on 9/28/15.
+//
+//
+
+import Foundation
+import XCTest
+
+class SocketPollingAcknowledgementTest: SocketAcknowledgementTest {
+    override func setUp() {
+        AbstractSocketTest.socket = AbstractSocketTest.regularPollingSocket
+        testKind = TestKind.Acknowledgement
+        openConnection(AbstractSocketTest.socket)
+    }
+}
