6b219c32e2bd7e6cd43bff2713d314f86d63361a
Lukas Schmidt <lukas.schmi@gmail.com>
2015-10-06 10:10:37 +0200

    refactor

--- SocketIO-MacTests/SocketPollingEmitTest.swift
+++ SocketIO-MacTests/SocketPollingEmitTest.swift
@@ -9,10 +9,67 @@
 import Foundation
 import XCTest
 
-class SocketPollingEmitTest: SocketEmitTest {
+class SocketPollingEmitTest: AbstractSocketTest {
     override func setUp() {
         AbstractSocketTest.socket = AbstractSocketTest.regularPollingSocket
         testKind = TestKind.Emit
         openConnection(AbstractSocketTest.socket)
+    }
+    
+    func testConnectionStatus() {
+        super.checkConnectionStatus()
+    }
+    
+    func testBasic() {
+        SocketTestCases.testBasic(socketEmit)
+    }
+    
+    func testNull() {
+        SocketTestCases.testNull(socketEmit)
+    }
+    
+    func testBinary() {
+        SocketTestCases.testBinary(socketEmit)
+    }
+    
+    
+    func testArray() {
+        SocketTestCases.testArray(socketEmit)
+    }
+    
+    func testString() {
+        SocketTestCases.testString(socketEmit)
+    }
+    
+    func testBool() {
+        SocketTestCases.testBool(socketEmit)
+    }
+    
+    func testInteger() {
+        SocketTestCases.testInteger(socketEmit)
+    }
+    
+    func testDouble() {
+        SocketTestCases.testDouble(socketEmit)
+    }
+    
+    func testJSON() {
+        SocketTestCases.testJSON(socketEmit)
+    }
+    
+    func testJSONWithBuffer() {
+        SocketTestCases.testJSONWithBuffer(socketEmit)
+    }
+    
+    func testUnicode() {
+        SocketTestCases.testUnicode(socketEmit)
+    }
+    
+    func testMultipleItems() {
+        SocketTestCases.testMultipleItems(socketMultipleEmit)
+    }
+    
+    func testMultipleWithBuffer() {
+        SocketTestCases.testMultipleItemsWithBuffer(socketMultipleEmit)
     }
 }
