0959dd35d72bbb8bdf3000f87aeb82c9eb77119c
Erik <nuclear.ace@gmail.com>
2016-05-04 09:30:55 -0400

    change author name

--- Source/String.swift
+++ Source/String.swift
@@ -2,7 +2,7 @@
 //  String.swift
 //  Socket.IO-Client-Swift
 //
-//  Created by Erik Little on 9/16/15.
+//  Created by Yannick Loriot on 5/4/16.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
