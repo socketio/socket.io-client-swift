23c76417d206b865e13b3e76297b9cf308d521f2
Erik <nuclear.ace@gmail.com>
2017-05-05 22:35:39 -0400

    Fix #667 Make no ack an enum case

--- SocketIO-MacTests/SocketAckManagerTest.swift
+++ SocketIO-MacTests/SocketAckManagerTest.swift
@@ -9,18 +9,43 @@
 import XCTest
 @testable import SocketIO
 
-class SocketAckManagerTest: XCTestCase {
+class SocketAckManagerTest : XCTestCase {
     var ackManager = SocketAckManager()
 
     func testAddAcks() {
-        let callbackExpection = self.expectation(description: "callbackExpection")
+        let callbackExpection = expectation(description: "callbackExpection")
         let itemsArray = ["Hi", "ho"]
+
         func callback(_ items: [Any]) {
             callbackExpection.fulfill()
         }
+
         ackManager.addAck(1, callback: callback)
         ackManager.executeAck(1, with: itemsArray, onQueue: DispatchQueue.main)
-        
+
         waitForExpectations(timeout: 3.0, handler: nil)
+    }
+
+    func testManagerTimeoutAck() {
+        let callbackExpection = expectation(description: "Manager should timeout ack with noAck status")
+        let itemsArray = ["Hi", "ho"]
+
+        func callback(_ items: [Any]) {
+            XCTAssertEqual(items.count, 1, "Timed out ack should have one value")
+            guard let timeoutReason = items[0] as? String else {
+                XCTFail("Timeout reason should be a string")
+
+                return
+            }
+
+            XCTAssertEqual(timeoutReason, SocketAckStatus.noAck.rawValue)
+
+            callbackExpection.fulfill()
+        }
+
+        ackManager.addAck(1, callback: callback)
+        ackManager.timeoutAck(1, onQueue: DispatchQueue.main)
+
+        waitForExpectations(timeout: 0.2, handler: nil)
     }
 }
