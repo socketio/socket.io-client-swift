f00af672835ab665a2af31df76b7b114e158a5b8
Erik <nuclear.ace@gmail.com>
2017-07-08 09:33:43 -0400

    Use ibm commoncrypto

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "SocketIO",
     dependencies: [
         .Package(url: "https://github.com/daltoniam/zlib-spm.git", majorVersion: 1),
-        .Package(url: "https://github.com/daltoniam/common-crypto-spm.git", majorVersion: 1)
+        .Package(url: "https://github.com/IBM-Swift/CommonCrypto.git", majorVersion: 0),
     ]
 )
