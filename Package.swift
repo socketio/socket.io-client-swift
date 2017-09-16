afc5a561d0f43bfb7a35ad98a670ec763c02f65a
Erik Little <nuclear.ace@gmail.com>
2017-09-16 11:08:16 -0400

    Start work on linux

--- Package.swift
+++ Package.swift
@@ -1,9 +1,15 @@
 import PackageDescription
 
+let deps: [Package.Dependency]
+
+#if !os(Linux)
+deps = [.Package(url: "https://github.com/nuclearace/Starscream", majorVersion: 8)]
+#else
+deps = [.Package(url: "https://github.com/vapor/engine", majorVersion: 2)]
+#endif
+
 let package = Package(
     name: "SocketIO",
-    dependencies: [
-        .Package(url: "https://github.com/nuclearace/Starscream", majorVersion: 8),
-    ],
+    dependencies: deps,
     exclude: ["Source/Starscream"]
 )
