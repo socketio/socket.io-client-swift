bf2beb7431ccd553a90ab0a86c6df7ed2a078fd9
Erik Little <nuclear.ace@gmail.com>
2017-09-19 21:10:32 -0400

    see if this fixes spm

--- Package.swift
+++ Package.swift
@@ -4,10 +4,13 @@ import PackageDescription
 
 let package = Package(
     name: "SocketIO",
+    products: [
+        .library(name: "SocketIO", targets: ["SocketIO"])
+    ],
     dependencies: [
         .package(url: "https://github.com/nuclearace/Starscream", from: "8.0.0"),
     ],
     targets: [
-        .target(name: "SocketIO", exclude: ["Sources/Starscream"])
+        .target(name: "SocketIO", dependencies: ["StarscreamSocketIO"], exclude: ["Sources/Starscream"])
     ]
 )
